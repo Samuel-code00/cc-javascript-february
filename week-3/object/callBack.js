f909e528dc1832073a75c2af8a8cc4679240a050
Samuel-code00 <samphabz00@gmail.com>
2024-03-20 12:40:59 +0100

    Modefied version of callback function

--- week-3/object/callBack.js
+++ week-3/object/callBack.js
@@ -6,16 +6,7 @@
 
 const myNumbers = [4, 1, -20, -7, 5, 9, -6];
 
+ 
 const positiveNumbers = removeNegative(myNumbers, (x) => x >= 0);
 
 document.getElementById("callback").innerHTML = positiveNumbers;
-
-function removeNegative(numbers, callback){
-    const myArray = [];
-    for (const x of numbers) {
-        if (callback(x)) {
-            myArray.push(x);
-        }
-    }
-    return myArray;
-}
